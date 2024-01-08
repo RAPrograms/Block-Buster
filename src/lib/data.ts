27d7e7c90e8314a115b751b491e9d2695572fb8f
Dog2657 <reeceya2006@gmail.com>
2024-01-08 20:38:47 +0000

    Added state management

---
+++ src/lib/data.ts
@@ -0,0 +1,86 @@
+import { get, writable } from 'svelte/store';
+
+export interface template{
+    id?: string
+    name: string,
+    grid_size: number,
+    questions: Record<string, Array<{answer: string, question: string}>>
+}
+
+export const database = new Promise<IDBDatabase>((resolve, reject) => {
+    const request = indexedDB.open("Games", 1);
+
+    request.onupgradeneeded = (event) => {
+        //@ts-ignore
+        const upgradeDB: IDBDatabase = event.target.result;
+
+        if(!upgradeDB.objectStoreNames.contains("templates"))
+            upgradeDB.createObjectStore("templates", { keyPath: "id" });
+    }
+
+    request.onerror = (event) => {
+        reject(event)
+    };
+    request.onsuccess = (event) => {
+        //@ts-ignore
+        resolve(event.target.result)
+    }
+})
+
+
+export const templateStore = (() => {
+    const store = writable(new Promise<Array<template>>(async (resolve, reject) => {
+        const transaction = (await database).transaction(["templates"], "readwrite");
+        const action = transaction.objectStore("templates").getAll()
+
+        action.onerror = () => {reject()}
+        action.onsuccess = ({target}) => {
+            //@ts-ignore
+            resolve(target.result)
+        }
+    }));
+
+    const getById = (id: string) => {
+        //TODO: add type
+        return new Promise<template | null>(async (resolve, reject) => {
+            const transaction = (await database).transaction(["templates"], "readwrite");
+            const action = transaction.objectStore("templates").get(id)
+
+            action.onerror = () => {resolve(null)}
+            action.onsuccess = ({target}) => {
+                //@ts-ignore
+                resolve(target.result)
+            }
+        })
+    }
+
+    return {
+		subscribe: store.subscribe,
+        get: getById,
+        add: async (data: template) => {
+            let id = crypto.randomUUID()
+            while(await getById(id)){ id = crypto.randomUUID() }
+            data.id = id
+
+            const transaction = (await database).transaction(["templates"], "readwrite");
+            transaction.objectStore("templates").add(data)
+        },
+        set: async (id: string, data: template) => {
+            data.id = id
+
+            const transaction = (await database).transaction(["templates"], "readwrite");
+            transaction.objectStore("templates").put(data)
+
+            const storeData = await get(store)
+            for (const index in storeData) {
+                if(storeData[index].id !== id)
+                    continue
+
+                storeData[index] = data
+                break
+            }
+
+            store.set(Promise.resolve(storeData))
+        }
+	};
+})()
